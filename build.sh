e266cf2c2dca0203d22596b6d8d62bb37fccc3f4
Pau Rodriguez-Estivill <prodrigestivill@gmail.com>
2021-03-07 12:09:30 +0100

    Add more targets

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
-TARGETS="linux-amd64 linux-arm64 linux-arm linux-386 darwin-amd64 windows-amd64"
-STATIC_TARGETS="linux-amd64 linux-arm64 linux-arm linux-386"
+TARGETS="linux-amd64 linux-arm64 linux-arm linux-386 linux-s390x linux-ppc64le darwin-amd64 darwin-arm64 windows-amd64"
+STATIC_TARGETS="linux-amd64 linux-arm64 linux-arm linux-386 linux-s390x linux-ppc64le"
 BUILD=`git rev-parse --short HEAD`
 VERSION=`git describe --tags`
 MAIN_GO="main.go"
